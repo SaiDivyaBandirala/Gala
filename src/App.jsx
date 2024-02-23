83176fcd94d78e408e0c3fc530d617af40f7fd2d
SaiDivya <bsaidivya97@gmail.com>
2024-02-23 23:05:53 +0100

    Added getUsersList and getUsers API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React, { useContext } from "react";
+import React, { useEffect } from "react";
 import { Button, Typography } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { DataContext } from "./contexts/DataContext";
 import "./App.css";
-import { fetchMetaData } from "./api/Api";
+import { fetchMetaData, getUser, getUsersList } from "./api/Api";
 
 const theme = createTheme({
     palette: {
@@ -32,23 +31,26 @@ const theme = createTheme({
 });
 
 function App() {
-    const { usersList } = useContext(DataContext);
-    const [users, setUsers] = useState([]);
-
     useEffect(() => {
-        fetchMetaData()
-            .then((data) => {
-                console.log(data.data.users);
-                setUsers(data.data.users);
-            })
-            .catch((error) => {
-                console.error("Error fetching metadata:", error);
-            });
-    }, [users]);
+        const fetchData = async () => {
+            try {
+                const metaDataResponse = await fetchMetaData();
+                const usersListResponse = await getUsersList(metaDataResponse);
+
+                usersListResponse.data.map((user) => {
+                    getUser(user);
+                });
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
     return (
         <ThemeProvider theme={theme}>
             <Typography>GALA</Typography>
-            <Typography>{users}</Typography>
+            <Typography></Typography>
             <Button variant="contained">Theme</Button>
         </ThemeProvider>
     );
