6c1e3364a31e87b56cc972e0514b9f4479516de6
Ajay <ajayraj2187@gmail.com>
2024-05-11 09:37:26 +0200

    Login Regirects works, Conditional renders for logins

--- src/pages/Dashboard/subs/navbar.jsx
+++ src/pages/Dashboard/subs/navbar.jsx
@@ -2,23 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { AppBar, Toolbar, Button, Typography } from "@mui/material";
 import { NavBarButtons } from "../../../components/buttons/nav-bar-buttons";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Navbar() {
+    const { isAuthenticated } = useAuth0();
+
     return (
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     GALA
                 </Typography>
-                <Button component={Link} to="/" color="inherit">
-                    Dashboard
-                </Button>
-                <Button component={Link} to="/games" color="inherit">
-                    Games
-                </Button>
-                <Button component={Link} to="/sevens" color="inherit">
-                    Sevens
-                </Button>
+                {isAuthenticated && (
+                    <>
+                        <Button component={Link} to="/" color="inherit">
+                            Dashboard
+                        </Button>
+                        <Button component={Link} to="/games" color="inherit">
+                            Games
+                        </Button>
+                        <Button component={Link} to="/sevens" color="inherit">
+                            Sevens
+                        </Button>
+                    </>
+                )}
+
                 <NavBarButtons></NavBarButtons>
             </Toolbar>
         </AppBar>
