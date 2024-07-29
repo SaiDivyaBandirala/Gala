ca7b8c8d428dd6ddd96fd005e7524ddb82a12a77
Ajay <ajayraj2187@gmail.com>
2024-07-29 19:52:53 +0200

    Moving navigation to separate file for edit game

--- src/pages/EditGame/index.jsx
+++ src/pages/EditGame/index.jsx
@@ -1,10 +1,10 @@
-import { Typography } from "@mui/material";
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import Navbar from "../Dashboard/subs/navbar";
 import BaseContainer from "../../components/containers/base-contatiner";
 import { DataContext } from "../../contexts/DataContext";
 import Carousel from "../../components/containers/carousel";
 import CSlider from "../../components/sliders/circular-slider";
+import Nav from "./subs/Nav";
 
 const EditGame = () => {
     const { gamesList, selectedGame } = useContext(DataContext);
@@ -13,13 +13,12 @@ const EditGame = () => {
     const roundsArr = Array.from(
         { length: Object.keys(currentGame.data.rounds).length },
         (_, index) => index + 1
-    );
-
+    ).map((_) => "Round " + _);
     return (
         <>
             <Navbar></Navbar>
             <BaseContainer>
-                <Typography>EditGame</Typography>
+                <Nav />
                 <Carousel items={playersArr} />
                 <Carousel items={roundsArr} />
                 <CSlider></CSlider>
---
+++ src/pages/EditGame/subs/Nav.jsx
@@ -0,0 +1,28 @@
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { ButtonBase, Stack, Typography } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Nav() {
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        navigate("/sevens");
+    };
+
+    return (
+        <Stack direction="row" sx={{ padding: "1em" }}>
+            <Typography variant="h4" sx={{ flexGrow: 1 }}>
+                <ButtonBase
+                    onClick={() => handleBack()}
+                    sx={{ padding: "0 1em" }}
+                >
+                    <ArrowBackIcon />
+                </ButtonBase>
+            </Typography>
+            <Typography variant="h4" sx={{ align: "center", flexGrow: 1 }}>
+                Edit Scores
+            </Typography>
+        </Stack>
+    );
+}
